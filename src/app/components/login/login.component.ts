bf12ee928b27a5fd63f5b0de41d8de010604ff1e
jwu02 <jiacongwu31@gmail.com>
2023-10-01 23:49:48 +0800

    Login failure message

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -21,6 +21,8 @@ export class LoginComponent {
     private router: Router,
     private userService: UserService
   ) {
+    // store registration success status for redirected page for 
+    // conditionally displaying registration success status on login page
     // https://stackoverflow.com/questions/58556569/angular-7-router-navigate-with-message-after-redirecting
     const navigation = this.router.getCurrentNavigation();
     if (navigation) {
@@ -29,22 +31,39 @@ export class LoginComponent {
         this.registrationSuccess = state.registrationSuccess;
       }
     }
+
+    // redirect to home page if user try to access login page while logged in
+    if (sessionStorage.getItem("loggedInUser")) {
+      this.router.navigate(['/posts']).then(() =>
+        window.location.reload()
+      );
+    }
   }
 
   loginUser(): void {
     if (this.loginForm.valid) {
       let loginDetails = this.loginForm.value;
 
-      this.userService.loginUser(loginDetails)
-        .subscribe(res => {
-          console.log(res);
-          if (res.status) {
-            sessionStorage.setItem('loggedInUser', JSON.stringify(res.user));   // if it's object
-            this.router.navigate(['posts']).then(() =>
-              window.location.reload()
-            );
+      this.userService.loginUser(loginDetails).subscribe(res => {
+        console.log(res);
+
+        if (res.status) { // login success
+          // store the logged in user in session storage and redirect to home page
+          sessionStorage.setItem('loggedInUser', JSON.stringify(res.user));
+          this.router.navigate(['posts']).then(() => window.location.reload());
+        } else { // login failure
+          // display failure message on login page
+          const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+          if (alertPlaceholder) {
+            alertPlaceholder.innerHTML = [
+              `<div class="alert alert-danger alert-dismissible" role="alert">`,
+              `   <div>${res.message}</div>`,
+              '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+              '</div>'
+            ].join('');
           }
-        });
+        }
+      });
     }
   }
 }
